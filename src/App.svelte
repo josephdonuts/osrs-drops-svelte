8bcc1c4a382702254c924b41dea1c2b20fcbc4e1
Joey Muller <joeemuller@gmail.com>
2023-01-07 07:38:10 -0500

    componentized, still needs css formatting

--- src/App.svelte
+++ src/App.svelte
@@ -9,40 +9,14 @@
 		date="Jan 2, 2023"
 		src="images/easyclue.png"
 		></Post>
-
-        <!-- <div class="post">
-            <div class="title">
-                <h1 class="drop-title">
-                    easy clue drop
-                </h1>
-                <p class="date">
-                    Jan 2, 2023
-                </p>
-            </div>
-            <img class="img" src="images/easyclue.png" alt="easy clue">
-        </div> -->
-
-        <div class="post">
-            <div class="title">
-                <h1 class="drop-title">
-                    hydra's claw #2
-                </h1>
-                <p class="date">
-                    Dec 29, 2022
-                </p>
-            </div>
-            <img class="img" src="images/hydraclaw.png" alt="hydra claw">
-        </div>
-        <div class="post">
-            <div class="title">
-                <h1 class="drop-title">
-                    primordials
-                </h1>
-                <p class="date">
-                    Sept 17, 2022
-                </p>
-            </div>
-            <img class="img" src="images/primordial.png" alt="prims">
-        </div>
+        <Post
+        title="hydra's claw #2"
+        date="Dec 29, 2022"
+        src="images/hydraclaw.png"
+        ></Post>
+        <Post
+        title="primordials"
+        date="Sept 17, 2022"
+        src="images/primordial.png"></Post>
     </div>
 </main>
--- src/Post.svelte
+++ src/Post.svelte
@@ -5,11 +5,44 @@
 </script>
 
 <article class="post">
-  <h1 class="post-title">
-      <span class="title">{title}</span>
-  </h1>
-  <p class="date">
-      <span class="date">{date}</span>
-  </p>
+  <div class="post-title-date">
+    <h1 class="post-title">
+        {title}
+    </h1>
+    <p class="date">
+      {date}
+    </p>
+  </div>
   <img class="img" {src} alt="">
 </article>
+
+<style>
+.post {
+  display: flex;
+  border: 1px solid black;
+  background-color: rgb(196, 173, 142);
+  border-radius: 10px;
+  margin: 15px;
+  padding: 15px;
+  width: fit-content;
+}
+.post-title-date {
+  display: grid;
+  grid-template-columns: auto;
+}
+.post-title {
+  flex: 1;
+}
+.date {
+  flex: 1;
+  text-align: right;
+}
+.img {
+    display: flex;
+    align-content: center;
+    margin-left: auto;
+    margin-right: auto;
+    object-fit: fill;
+    border-radius: 10px;
+}
+</style>
