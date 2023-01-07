2d11400f1b7e6ffe8cbd062f2cf3e438980f4856
Joey Muller <joeemuller@gmail.com>
2023-01-07 07:08:06 -0500

    Ported code over to Svelte and componentized

---
+++ src/Post.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let title;
+  export let date;
+  export let src;
+</script>
+
+<article class="post">
+  <h1 class="post-title">
+      <span class="title">{title}</span>
+  </h1>
+  <p class="date">
+      <span class="date">{date}</span>
+  </p>
+  <img class="img" {src} alt="">
+</article>
